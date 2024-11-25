d1f1226aa52883469c1d8172432fbf3dabc8cfce
marceloalfonso <114462976+marceloalfonso@users.noreply.github.com>
2024-11-25 20:51:48 -0300

    Add new images to gallery

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -11,12 +11,7 @@ const Gallery = () => {
             <img src={require('../../assets/foto3.jpg')} alt='' />
             <img src={require('../../assets/foto4.jpg')} alt='' />
             <img src={require('../../assets/foto5.jpg')} alt='' />
-
-            <img src={require('../../assets/foto1.jpg')} alt='' />
-            <img src={require('../../assets/foto2.jpg')} alt='' />
-            <img src={require('../../assets/foto3.jpg')} alt='' />
-            <img src={require('../../assets/foto4.jpg')} alt='' />
-            <img src={require('../../assets/foto5.jpg')} alt='' />
+            <img src={require('../../assets/foto6.jpg')} alt='' />
           </div>
         </div>
       </div>
